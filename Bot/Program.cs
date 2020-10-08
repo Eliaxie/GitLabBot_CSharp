f08678ab3810bfcf2277417549c227d5e1200632
Elia Maggioni <elia.maggioni@gmail.com>
2020-10-08 10:31:46 +0200

    db, query

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,9 +1,16 @@
 ﻿using Bot;
+using PoliNetworkBot_CSharp.Code.Utils;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Bot
@@ -11,20 +18,74 @@ namespace Bot
     class Program
     {
         static TelegramBotClient botClient = null;
-        
 
         static Dictionary<long, Conversation> dict = new Dictionary<long, Conversation>();
 
         static void Main(string[] args)
         {
             botClient = new TelegramBotClient("1307723925:AAGoudgP99mVb0BWFlggHojxyJWi5psbfbU");
-            botClient.OnMessage += BotClient_OnMessageAsync;         
+            botClient.OnMessage += BotClient_OnMessageAsync;
+            botClient.OnCallbackQuery += BotOnCallbackQueryReceived;
             botClient.StartReceiving();
+            Thread t = new Thread(Checkmessage);
+            t.Start();
             Console.ReadLine();
+        }
+
+        private async static void Checkmessage()
+        {
+            while (true)
+            {
+                string q = "SELECT * FROM main.FILE WHERE Approved = 'N'";
+                System.Data.DataTable r = SqLite.ExecuteSelect(q);
+                if (r != null && r.Rows.Count > 0)
+                {
+                    foreach (DataRow dr in r.Rows)
+                    {
+                        string pathFile = (string)dr["Path"];
+                        System.IO.File.Delete(pathFile);
+
+                        string q2 = "UPDATE main.FILE SET Approved = 'C' WHERE Id_message = " + ((int)dr["Id_message"]).ToString();
+                        SqLite.ExecuteSelect(q2);
+                    }
+                }
+                Thread.Sleep(1000 * 1);
+            }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+            switch (callbackQuery.Data)
+            {
+                case "y":
+                    {
+                        await botClient.AnswerCallbackQueryAsync(callbackQueryId: callbackQuery.Id, text: $"Modification Accepted");
+                        botClient.EditMessageTextAsync(callbackQuery.Message.Chat.Id, callbackQuery.Message.MessageId, "<b>MERGED</b>", ParseMode.Html);
+                        var fileName = @"C:\Repos\doc\" + callbackQuery.Message.ReplyToMessage.Document.FileName;
+                        using FileStream fileStream = System.IO.File.OpenWrite(fileName);
+                        await botClient.GetInfoAndDownloadFileAsync(callbackQuery.Message.ReplyToMessage.Document.FileId, destination: fileStream);
+                        //  await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "File Saved in " + fileName);
+                        //  InputOnlineFile inputOnlineFile = new InputOnlineFile(fileStream, e.Message.Document.FileName);
+                        //  await botClient.SendDocumentAsync(-1001403617749, inputOnlineFile);
+                        //  using (var sendFileStream = File.Open(fileName, FileMode.Open))
+                        string q = "SELECT * FROM main.FILE WHERE Id_message = " + callbackQuery.Message.ReplyToMessage.Document.FileId.ToString();
+                        System.Data.DataTable r = SqLite.ExecuteSelect(q);
+                        string q2 = "UPDATE main.FILE SET Approved = 'Y' WHERE Id_message = " + callbackQuery.Message.ReplyToMessage.Document.FileId.ToString();
+                        SqLite.ExecuteSelect(q2);
+                    }
+                    break;
+                case "n":
+                    await botClient.AnswerCallbackQueryAsync(callbackQueryId: callbackQuery.Id, text: $"Modification Denied");
+                    botClient.EditMessageTextAsync(callbackQuery.Message.Chat.Id ,callbackQuery.Message.MessageId, "<b>DENIED</b>", ParseMode.Html);
+                    break;
+            }
         }
         private static async void BotClient_OnMessageAsync(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             Console.WriteLine(e.Message.Text);
+            Conversation conv = new Conversation();
+            dict.TryAdd(e.Message.From.Id, conv);
             switch (dict[e.Message.From.Id].getStato())
             {
                 case 1:
@@ -53,21 +114,58 @@ namespace Bot
                     botClient.SendTextMessageAsync(e.Message.Chat.Id, "Rinserisci");
                     break;
             }
-            await botClient.ForwardMessageAsync(e.Message.Chat.Id, e.Message.Chat.Id, e.Message.MessageId);
-            
+            try {
+                Message messageFW = await botClient.ForwardMessageAsync(-1001403617749, e.Message.Chat.Id, e.Message.MessageId);
+                List<InlineKeyboardButton> inlineKeyboardButton = new List<InlineKeyboardButton>() {
+                    new InlineKeyboardButton() {Text = "Yes", CallbackData = "y" },
+                    new InlineKeyboardButton() {Text = "No", CallbackData = "n" },
+                };        
+                InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup(inlineKeyboardButton);
+                Message queryAW = await botClient.SendTextMessageAsync(-1001403617749, "Approvi l'inserimento del documento in %PATH%?", ParseMode.Default, false, false, messageFW.MessageId, inlineKeyboardMarkup, default(CancellationToken));
+                CallbackQuery query = new CallbackQuery();
+                await botClient.AnswerCallbackQueryAsync(query.Id, "File Inserito");
+                var fileName = @"C:\Repos\doc\" + e.Message.Document.FileName;
+                using FileStream fileStream = System.IO.File.OpenWrite(fileName);
+                //   await botClient.SendTextMessageAsync(e.Message.Chat.Id, e.Message.Document.FileId);
+                await botClient.GetInfoAndDownloadFileAsync(e.Message.Document.FileId, destination: fileStream);
+                await botClient.SendTextMessageAsync(e.Message.Chat.Id, "File Saved in " + fileName);
+                //  InputOnlineFile inputOnlineFile = new InputOnlineFile(fileStream, e.Message.Document.FileName);
+                //  await botClient.SendDocumentAsync(-1001403617749, inputOnlineFile);
+                //  using (var sendFileStream = File.Open(fileName, FileMode.Open))
+                string q = "INSERT INTO Main.FILE (Path, Id_owner, Data, Approved, Id_cs, Desc, Id_message) VALUES (@p, @io, @d, @a, @ic, @d, @im)";
+                Dictionary<string, object> keyValuePairs = new Dictionary<string, object>() {
+                    {"@p", fileName },
+                    {"@io", e.Message.From.Id },
+                    {"@a", "Q" },
+                    {"@d", DateTime.Now},
+                    {"@ic", 1},
+                    {"@d", "Default Desc"},
+                    {"@im", e.Message.Document.FileId}
+                };
+                SqLite.Execute(q, keyValuePairs);
+                fileStream.Close();
+                //using FileStream fileStream1 = System.IO.File.OpenRead(fileName);
+            }
+            catch { }
+
+        }
+
+        private static void BotClient_OnCallbackQuery(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
 
 
 
-/*List<InlineKeyboardButton> keyboardButtons = new List<InlineKeyboardButton>() { 
-new InlineKeyboardButton() { Text = "si" },
-                new InlineKeyboardButton() { Text = "no" }
-            };
 
-Telegram.Bot.Types.ReplyMarkups.IReplyMarkup replyMarkup = new InlineKeyboardMarkup(keyboardButtons);
-botClient.SendTextMessageAsync(e.Message.Chat.Id, "Ti piace?", Telegram.Bot.Types.Enums.ParseMode.Default, true, true, e.Message.MessageId, replyMarkup);
+//fileStream1.Seek(0, SeekOrigin.Begin);
+//await botClient.SendDocumentAsync(-1001403617749, new Telegram.Bot.Types.InputFiles.InputOnlineFile(fileStream1, fileName));
+//await botClient.SendTextMessageAsync(e.Message.Chat.Id, "File Sent");
+
+/*List<InlineKeyboardButton> keyboardButtons = new List<InlineKeyboardButton>() { 
+
 
 switch (e.Message.Type)
 {
