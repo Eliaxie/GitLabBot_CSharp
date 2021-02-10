b6cf2f96b2f12122c2dd762f3ae2f4839e81f09f
Elia Maggioni <elia.maggioni@gmail.com>
2021-02-10 23:20:20 +0100

    .

---
+++ ChannelsForApproval.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Bot.Enums
+{
+    class ChannelsForApproval
+    {
+        static ChatId defaultChannel = 0;
+        static ChatId matNanoChannel = -1001227167038;
+        static ChatId infoChannel = -1001422638605;
+        static ChatId mobilityMDChannel = -1001422638605;
+        static ChatId debug = -1001403617749;
+
+        internal static ChatId getChannel(string v)
+        {
+            switch (v.ToLower())
+            {
+                case "matnano":
+                    return ChannelsForApproval.matNanoChannel;
+                    break;
+                case "info":
+                    return ChannelsForApproval.infoChannel;
+                    break;
+                case "mobilitymd":
+                    return ChannelsForApproval.mobilityMDChannel;
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    return ChannelsForApproval.defaultChannel;
+            }
+        }
+    }
+}
