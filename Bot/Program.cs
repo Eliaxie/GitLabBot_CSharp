d3977e0e4e8327a62b4e83f885ea6b6afc0d096f
Eliaxie <elia.maggioni@gmail.com>
2021-08-18 11:06:52 +0200

    Update Program.cs

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -161,7 +161,7 @@ namespace Bot
                         Console.WriteLine(whatChanged(e));
                         botClient.SendTextMessageAsync(-1001399914655, "Log: WhatChanged: " + whatChanged(e) + System.Environment.NewLine);
                         botClient.SendTextMessageAsync(-1001399914655, "Log: Commit: " + @"git commit -m 'git commit by bot updated file: " + whatChanged(e) + @"' --author=""PoliNetworkBot <polinetwork2@gmail.com>""");
-                        string commit = @"git commit -m 'git commit by bot updated file: " + whatChanged(e) + @"' --author=""polinetwork2@gmail.com""";
+                        string commit = @"git commit -m 'git commit by bot updated file: " + whatChanged(e) + @"' --author=""PoliBot <polinetwork2@gmail.com>""";
                         powershell.AddScript(commit);
                         results = powershell.Invoke().ToList();
                         for (int i = 0; i < results.Count(); i++)
